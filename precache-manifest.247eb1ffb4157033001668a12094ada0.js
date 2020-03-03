855286439bf6098f98a7695c016a7eb138b9ac99
tvanrijn <vanrijn.tineke@gmail.com>
2020-03-03 23:23:10 +0100

    Updates

---
+++ precache-manifest.247eb1ffb4157033001668a12094ada0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e12bca79be22b9e529d5750143a576d6",
+    "url": "/star-wars-planets/index.html"
+  },
+  {
+    "revision": "a6735a63cfb6c20b17f1",
+    "url": "/star-wars-planets/static/css/2.299a5230.chunk.css"
+  },
+  {
+    "revision": "3d3520b12bbe4002cf7f",
+    "url": "/star-wars-planets/static/css/main.a615ea94.chunk.css"
+  },
+  {
+    "revision": "a6735a63cfb6c20b17f1",
+    "url": "/star-wars-planets/static/js/2.b049141d.chunk.js"
+  },
+  {
+    "revision": "e468b20662ce7acec401182a9064b329",
+    "url": "/star-wars-planets/static/js/2.b049141d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d3520b12bbe4002cf7f",
+    "url": "/star-wars-planets/static/js/main.72cba829.chunk.js"
+  },
+  {
+    "revision": "e04384cc20b1444bad3f",
+    "url": "/star-wars-planets/static/js/runtime-main.0ea7e990.js"
+  },
+  {
+    "revision": "764717d66ec16b37b687ddf28f842ee7",
+    "url": "/star-wars-planets/static/media/Cooljazz.764717d6.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/star-wars-planets/precache-manifest.0cd7aaddc0ce4ee9e3c88b013f535129.js"
+  "/star-wars-planets/precache-manifest.247eb1ffb4157033001668a12094ada0.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/star-wars-planets/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/star-wars-planets/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
