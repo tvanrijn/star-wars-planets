a0612f830baee8b91946d9c727876a4e27882deb
tvanrijn <vanrijn.tineke@gmail.com>
2020-03-03 23:22:35 +0100

    use graphql for fetching planet data

---
+++ src/components/LoadingBar/LoadingBar.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./LoadingBar.css";
+
+const LoadingBar = () => (
+  <div className="tc">
+    <h1 className="loadingBar">Loading</h1>
+  </div>
+);
+
+export default LoadingBar;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './containers/App';
-import * as serviceWorker from './serviceWorker';
-import 'tachyons';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./containers/App";
+import * as serviceWorker from "./serviceWorker";
+import "tachyons";
+import { ApolloProvider } from "react-apollo";
+import { createHttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { ApolloClient } from "apollo-boost";
+
+const link = createHttpLink({
+  uri: "https://starwars-graphql.herokuapp.com/"
+});
+
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({
+  link,
+  cache
+});
 
 ReactDOM.render(
-    <App />, 
-    document.getElementById('root')
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById("root")
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
